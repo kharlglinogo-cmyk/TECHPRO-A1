188947e44de0e0ae867658bc5d5bcac1d9d09b68
kharlglinogo-cmyk <kharlglinogo@gmail.com>
2025-09-05 07:14:37 +0000

    done

--- SEATWORK5.java
+++ SEATWORK5.java
@@ -1,1 +1,14 @@
+public class SEATWORK5 {
+        public static void main(String[] args) {
+                System.out.println("Seatwork5");
+                        int countdown = 3;
+
+                                while (countdown > 0) {
+                                            System.out.println(countdown);
+                                                        countdown--;
+                                                                }
+
+                                                                        System.out.println("Happy new year!!!");
+                                                                            }
+                                                                            }
 
