0fa43c8260d0d5d4e56e68f6cdf116186ece3e24
kharlglinogo-cmyk <kharlglinogo@gmail.com>
2025-09-05 08:00:05 +0000

    done

--- SEATWORK9.java
+++ SEATWORK9.java
@@ -1,1 +1,19 @@
+public class Seatworknine {
 
+    public static void main(String[] args) {
+
+    int[] numbers = {10, 20, 30, 40, 50};
+
+    int index = 0;
+
+    while(index < 5) {
+
+    System.out.print("value of item: " + numbers[index]);
+
+    index++;
+
+
+
+    } System.out.print("\n");
+    }
+    }
