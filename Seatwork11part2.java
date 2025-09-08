29527cea566915c45b25682e022fe1cffe9dd9b6
kharlglinogo-cmyk <kharlglinogo@gmail.com>
2025-09-08 02:17:25 +0000

    done

--- Seatwork11part2.java
+++ Seatwork11part2.java
@@ -1,1 +1,11 @@
+public class Seatwork11part2 {
+    	public static void main(String[] args) {
+            		System.out.println("SEATWORK11 PART 2");
+                    		int[][] myNumbers = { {1, 4, 2}, {3, 6, 8}
+                        		};
+                                		myNumbers[1][2] = 9;
+                                        		System.out.println(myNumbers[1][2]); //Outputs 9 instead of 8
+                            }
+                        
+        }
 
