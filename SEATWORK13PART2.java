c3b821fc636410707ac5b165f3110774761d709a
kharlglinogo-cmyk <kharlglinogo@gmail.com>
2025-10-01 10:49:29 +0000

    done

--- SEATWORK13PART2.java
+++ SEATWORK13PART2.java
@@ -1,1 +1,15 @@
+public class Seatwork13part2 {
+        public static void sayHello(int count) {
+                if (count == 0) { // halting condition
+                            System.out.println("Done!!");
+                                    } else {
+                                                System.out.println("Hello");
+                                                            sayHello(count - 1); // recursive call
+                                                                    }
+                                                                        }
 
+                                                                            public static void main(String[] args) {
+                                                                                    sayHello(3); // prints Hello 3 times
+                                                                                        }
+                                                                                        }
+}
